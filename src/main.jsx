82f62aff9c409a43415d5cd3e5ca3b8477c4d98b
dfrost <dmitryfrost90@gmail.com>
2023-05-12 14:56:07 +0300

    refactoring providers

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,15 @@ import App from './App.jsx';
 import 'normalize.css';
 import './index.css';
 import { AuthProvider } from './context/auth_context.jsx';
-// import { DataProvider } from './context/data_contenxt.jsx';
-import { FilterProvider } from './context/filter_context.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      {/* <DataProvider> */}
       <Provider store={store}>
-        <FilterProvider>
-          <App />
-        </FilterProvider>
+        <App />
       </Provider>
-      {/* </DataProvider> */}
     </AuthProvider>
   </React.StrictMode>
 );
